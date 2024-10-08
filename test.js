fdb4987a7dd24310397320bc7edaff09733bef7a
farasnobary <faras.nobary@torontomu.ca>
2024-10-08 12:12:55 -0400

    updates
    updatess

--- test.js
+++ test.js
@@ -2,3 +2,32 @@ function promptName() {
     const userName = prompt("Please enter your name:");
     document.getElementById('nameDisplay').innerHTML = `Hello, ${userName}!`;
 }
+
+function enlargeImage() {
+    const image = document.getElementById('myImage');
+    image.style.width = (image.style.width === '200px') ? '400px' : '200px';
+}
+
+function toggleMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function navHover(element) {
+    element.style.color = "blue";
+}
+
+function navOut(element) {
+    element.style.color = "";
+}
